cf05eb2b5cef4b67008250a8b0e1223bf93bcc74
Jon Delano <jd31068@hotmail.com>
2023-11-02 06:14:53 -0400

    Add project files.

---
+++ VBWinformCSVXML/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtExcelFile = New TextBox()
+        btnSelectExcel = New Button()
+        btnSelectXML = New Button()
+        txtXMLFile = New TextBox()
+        Label2 = New Label()
+        btnRun = New Button()
+        lblStatus = New Label()
+        ofDLG = New OpenFileDialog()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(5, 10)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(132, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Select Excel File"
+        ' 
+        ' txtExcelFile
+        ' 
+        txtExcelFile.Location = New Point(8, 38)
+        txtExcelFile.Name = "txtExcelFile"
+        txtExcelFile.ReadOnly = True
+        txtExcelFile.Size = New Size(730, 31)
+        txtExcelFile.TabIndex = 1
+        ' 
+        ' btnSelectExcel
+        ' 
+        btnSelectExcel.Location = New Point(744, 38)
+        btnSelectExcel.Name = "btnSelectExcel"
+        btnSelectExcel.Size = New Size(41, 30)
+        btnSelectExcel.TabIndex = 2
+        btnSelectExcel.Text = "..."
+        btnSelectExcel.UseVisualStyleBackColor = True
+        ' 
+        ' btnSelectXML
+        ' 
+        btnSelectXML.Location = New Point(747, 111)
+        btnSelectXML.Name = "btnSelectXML"
+        btnSelectXML.Size = New Size(41, 30)
+        btnSelectXML.TabIndex = 5
+        btnSelectXML.Text = "..."
+        btnSelectXML.UseVisualStyleBackColor = True
+        ' 
+        ' txtXMLFile
+        ' 
+        txtXMLFile.Location = New Point(11, 111)
+        txtXMLFile.Name = "txtXMLFile"
+        txtXMLFile.ReadOnly = True
+        txtXMLFile.Size = New Size(730, 31)
+        txtXMLFile.TabIndex = 4
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(8, 83)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(129, 25)
+        Label2.TabIndex = 3
+        Label2.Text = "Select XML File"
+        ' 
+        ' btnRun
+        ' 
+        btnRun.Location = New Point(656, 168)
+        btnRun.Name = "btnRun"
+        btnRun.Size = New Size(130, 36)
+        btnRun.TabIndex = 6
+        btnRun.Text = "Run"
+        btnRun.UseVisualStyleBackColor = True
+        ' 
+        ' lblStatus
+        ' 
+        lblStatus.Location = New Point(21, 179)
+        lblStatus.Name = "lblStatus"
+        lblStatus.Size = New Size(257, 32)
+        lblStatus.TabIndex = 7
+        ' 
+        ' ofDLG
+        ' 
+        ofDLG.RestoreDirectory = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 227)
+        Controls.Add(lblStatus)
+        Controls.Add(btnRun)
+        Controls.Add(btnSelectXML)
+        Controls.Add(txtXMLFile)
+        Controls.Add(Label2)
+        Controls.Add(btnSelectExcel)
+        Controls.Add(txtExcelFile)
+        Controls.Add(Label1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtExcelFile As TextBox
+    Friend WithEvents btnSelectExcel As Button
+    Friend WithEvents btnSelectXML As Button
+    Friend WithEvents txtXMLFile As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnRun As Button
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents ofDLG As OpenFileDialog
+End Class
---
+++ VBWinformCSVXML/Form1.vb
@@ -0,0 +1,135 @@
+﻿Imports System.IO
+Imports System.Xml
+Imports ClosedXML.Excel
+
+Public Class Form1
+    Private Sub UpdateStatusLabel(statusMsg As String)
+
+        ' this is just a message to indicate there is stuff going on
+        lblStatus.Text = statusMsg
+        Application.DoEvents()
+
+    End Sub
+
+    Private Sub FillDictionaryFromExcelFile(dict As Dictionary(Of String, String))
+
+        ' using CloseXML package, read the Excel file into the dictionary
+        Dim workbook As New XLWorkbook(txtExcelFile.Text)
+        Dim ws1 As IXLWorksheet = workbook.Worksheet(1)
+        Dim row As Int16 = 1
+        Dim idValue As String = ws1.Cell(row, 1).Value.ToString()
+
+        Do While idValue.Trim.Length > 0
+            dict.Add(idValue, idValue)
+            row += 1
+            idValue = ws1.Cell(row, 1).Value.ToString()
+        Loop
+
+        workbook.Dispose()
+
+    End Sub
+
+    Private Sub FillDictionaryFromCSVFile(dict As Dictionary(Of String, String))
+
+        ' open the id file (either Excel or CSV)
+        Dim idFile As New StreamReader(txtExcelFile.Text)
+
+        ' loop through the file and add the ID to the dictionary object
+        Dim idLine As String = ""
+        Do While Not idFile.EndOfStream
+            idLine = idFile.ReadLine
+            dict.Add(idLine, idLine)
+        Loop
+
+        idFile.Close()
+
+    End Sub
+    Private Sub btnSelectExcel_Click(sender As Object, e As EventArgs) Handles btnSelectExcel.Click
+
+        ' display an open file dialog to pick the excel file that contains the IDs
+        With ofDLG
+            .Filter = "Excel Files (*.xlsx) | *.xlsx | CSV Files (*.csv) | *.csv"
+            .Title = "Select ID File"
+            .FileName = "*.xlsx"
+        End With
+
+        If ofDLG.ShowDialog = DialogResult.Cancel Then Exit Sub
+
+        txtExcelFile.Text = ofDLG.FileName
+
+    End Sub
+
+    Private Sub btnSelectXML_Click(sender As Object, e As EventArgs) Handles btnSelectXML.Click
+
+        ' display an open file dialog to pick the XML file to comapre the IDs to
+        With ofDLG
+            .Filter = "XML Files (*.xlm) | *.xml"
+            .Title = "Select XML File"
+            .FileName = "*.xml"
+        End With
+
+        If ofDLG.ShowDialog = DialogResult.Cancel Then Exit Sub
+
+        txtXMLFile.Text = ofDLG.FileName
+    End Sub
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+
+        ' compare the selected file contents
+
+        ' check the text in the textbox that displays the selected excel file for the presense of
+        ' either xlsx or csv - if neither is found then this can't continue
+        If txtExcelFile.Text.ToUpper.IndexOf("XLSX") = -1 And txtExcelFile.Text.ToUpper.IndexOf("CSV") = -1 Then
+            MsgBox("No Excel / CSV file has been selected", MsgBoxStyle.Critical, "Missing File Selection")
+            Exit Sub
+        End If
+
+        ' check the text in the textbox that displays the selected xml file for the presense of
+        ' xmlm if it isn't found then this can't continue
+        If txtXMLFile.Text.ToUpper.IndexOf("XML") = -1 Then
+            MsgBox("No XML file has been selected", MsgBoxStyle.Critical, "Missing File Selection")
+            Exit Sub
+        End If
+
+        ' open the ID file and place the contents in a dictionary object for searching
+        UpdateStatusLabel("Reading ID File")
+        Dim idDict As New Dictionary(Of String, String)
+
+        If txtExcelFile.Text.ToUpper.IndexOf("XLSX") > 0 Then
+            ' an Excel file was selected
+            FillDictionaryFromExcelFile(idDict)
+        Else
+            FillDictionaryFromCSVFile(idDict)
+        End If
+
+        ' load the XML file into a document object, loop through each message element
+        ' search the dictionary created above, update the XML if required
+        UpdateStatusLabel("Comparing XML File")
+        Dim xmlFile As New XmlDocument()
+        Dim nodeList As XmlNodeList
+        Dim messageID As String
+
+        ' create a backup of the XML file, overwrite previous backup (if one exists)
+        File.Copy(txtXMLFile.Text, txtXMLFile.Text.Replace(".xml", ".backup"), True)
+
+        xmlFile.Load(txtXMLFile.Text)
+        nodeList = xmlFile.SelectNodes("//Message")
+
+        For Each nd As XmlElement In nodeList
+            messageID = nd.ChildNodes(0).InnerText
+
+            If idDict.ContainsKey(messageID) Then
+                ' the id was found in the file loaded previousle
+                ' set the indicator to 1
+                nd.ChildNodes(1).InnerText = "1"
+            Else
+                ' the id was not found - remove it from the xml file
+                nd.ParentNode.RemoveChild(nd)
+            End If
+        Next
+        xmlFile.Save(txtXMLFile.Text)
+
+        UpdateStatusLabel("Done")
+
+    End Sub
+End Class
